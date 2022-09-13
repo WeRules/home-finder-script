1b10c92a1a0582d70e9aa12c1bd7e2a34c84f1e9
Pablo Pirata <pablo@studocu.com>
2022-09-13 10:12:52 +0200

    typo in env variable

--- scraper.js
+++ scraper.js
@@ -32,7 +32,7 @@ console.log('pastResults:', pastResults);
 const results = {};
 
 const runTask = async () => {
-  const spreadsheet = await downloadSpreadsheetFile(process.env.GOOGLE_SPREADSHEET_ID, process.env.GATSBY_GOOGLE_SPREADSHEET_GID);
+  const spreadsheet = await downloadSpreadsheetFile(process.env.GOOGLE_SPREADSHEET_ID, process.env.GOOGLE_SPREADSHEET_GID);
   const workbook = read(await spreadsheet.arrayBuffer(), { type: 'array' });
   const sheetName = workbook.SheetNames[0];
   const spreadsheetData = utils.sheet_to_json(workbook.Sheets[sheetName], { raw: false });
